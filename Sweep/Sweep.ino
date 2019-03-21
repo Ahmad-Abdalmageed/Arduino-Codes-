5931f028b817d1f1c19977652a99094f5a794569
biomedicalengineerahmad <ahmedmageed19@gmail.com>
2019-03-21 15:46:09 +0200

    final task code

--- Sweep/Sweep.ino
+++ Sweep/Sweep.ino
@@ -11,21 +11,28 @@
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
-
+Servo myservo2;
 int pos = 0;    // variable to store the servo position
 
 void setup() {
   myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(9);
+  Serial.begin(9600);
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  int pos2 ;
+  for (pos = 0,pos2 = 180; pos <= 180,pos2>=0; pos += 10,pos2-=10) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    myservo.write(pos);
+    myservo2.write(pos2);
+    // tell servo to go to position in variable 'pos'
+    Serial.println(pos);
+    delay(1000);                       // waits 15ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo.write(pos);
+    myservo2.write(pos);// tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
--- Task_2_/Task_2_.ino
+++ Task_2_/Task_2_.ino
@@ -1,44 +1,38 @@
-// Servo Declarations 
+// Servo Declarations
 #include <Servo.h>
 Servo myservo_right;
-Servo myservo_left ; 
-//Sensors 
-int pressure = A0 ; 
+Servo myservo_left ;
+//Sensors
+int pressure = A0 ;
 int Vo ;
 
 int right_wing = 9 ;
+int pos_right = 90 ;
 int left_wing = 10 ;
-int pos = 0;
-//Gyro 
+int pos_left = 90 ;
+
+//Gyro
 #include<Wire.h>
-const int MPU_addr=0x68;
-int16_t axis_X,axis_Y,axis_Z;
-int minVal=265;
-int maxVal=402;
+const int MPU_addr = 0x68;
+int16_t axis_X, axis_Y, axis_Z;
+
+int minVal = 265;
+int maxVal = 402;
+
 double x;
 double y;
 double z;
 
 //Motors
-int motor = 6; 
-//Functions 
-double turn_right() 
+int motor = 6;
+//Functions
+void setup()
 {
-  myservo_right.write(-45) ;
-  myservo_left.write(-45) ; 
-}
-double turn_left() 
-{
-  myservo_left.write(-45) ;
-  myservo_right.write(-45) ; 
-}
- 
-void setup() 
-{
+  
   //Servo Attachment
   myservo_right.attach(right_wing);
   myservo_left.attach(left_wing);
-  
+
   //Gyro Attachment
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -46,48 +40,79 @@ void setup()
   Wire.write(0);
   Wire.endTransmission(true);
   Serial.begin(9600);
+  //MOTOR
+  pinMode(motor , OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-  //Gyro Logic 
   
+  //Sensor Logic
+  Vo = analogRead(pressure);
+  Serial.println (Vo);
+  
+  //Gyro Logic
+
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);
-  axis_X=Wire.read()<<8|Wire.read();
-  axis_Y=Wire.read()<<8|Wire.read();
-  axis_Z=Wire.read()<<8|Wire.read();
-  int xAng = map(axis_X,minVal,maxVal,-90,90);
-  int yAng = map(axis_Y,minVal,maxVal,-90,90);
-  int zAng = map(axis_Z,minVal,maxVal,-90,90);
-  x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
-  y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
-  z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+  Wire.requestFrom(MPU_addr, 14, true);
+  axis_X = Wire.read() << 8 | Wire.read();
+  axis_Y = Wire.read() << 8 | Wire.read();
+  axis_Z = Wire.read() << 8 | Wire.read();
+  int xAng = map(axis_X, minVal, maxVal, -90, 90);
+  int yAng = map(axis_Y, minVal, maxVal, -90, 90);
+  int zAng = map(axis_Z, minVal, maxVal, -90, 90);
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+  z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
   Serial.print("Angle of inclination in X axis = ");
   Serial.print(x);
   Serial.println((char)176);
   Serial.print("Angle of inclination in Y axis= ");
   Serial.print(y);
-  Serial.println((char)176);   
+  Serial.println((char)176);
   Serial.println("-------------------------------------------");
-  delay(1000);
+  delay(1000) ;  
+  if (Vo > 200 ) {
+    //LOGIC
+    if ( x >= 20 && x<=50 )
+    {
+      //Turn left
+      myservo_left.write(pos_left - 40) ;
+      myservo_right.write(pos_right -30) ;
+     }
+     else if (x >= 328 && x<=350)
+     {
+      //turn right 
+      myservo_left.write(pos_left +40);
+      myservo_right.write(pos_right+30);
+     }
+     else if (y >= 10 && y <= 30 ) 
+     { //pitch up
+      myservo_left.write(pos_left -30) ; 
+      myservo_right.write(pos_right+30);
+      }
+     else if (y >= 300 && y<= 350) 
+     {
+      //pitch down 
+      myservo_left.write(pos_left +50) ; 
+      myservo_right.write(pos_right-50);
+      } 
+     else {
+      myservo_left.write(pos_left);
+      myservo_right.write(pos_right);
+      }
+      //Fadel code el FSR
+      //Motor
+    }
+    //get above water
+    else if (Vo > 1000 ) {
+      //Get out 
+      
+      myservo_right.write(pos_right);
+      myservo_left.write(pos_left); 
+      } 
   
-  //Sensor Logic 
-  Vo = analogRead(pressure);
-  Serial.println (Vo);
-  
-  //Servos 
-  if (x > 45 ) 
-  {
-    turn_right() ;   
-  }
-  if (x < 45 )
-  {
-    turn_left() ; 
-  }
-  //Fadel code el FSR 
-  //Motor 
-  analogWrite(motor , 125 );
+    
 }
